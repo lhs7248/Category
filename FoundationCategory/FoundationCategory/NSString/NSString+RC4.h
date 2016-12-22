a9e165b14a3ccf677988b41adeda1f68d6d50c12
lhs724 <lhs7248@126.com>
2016-12-22 12:01:30 +0800

    RC4

--- FoundationCategory/FoundationCategory/NSString/NSString+RC4.h
+++ FoundationCategory/FoundationCategory/NSString/NSString+RC4.h
@@ -9,5 +9,5 @@
 #import <Foundation/Foundation.h>
 
 @interface NSString (RC4)
-
+-(NSString*) rc4Key:(NSString*) key str:(NSString*) str;
 @end
--- FoundationCategory/FoundationCategory/NSString/NSString+RC4.m
+++ FoundationCategory/FoundationCategory/NSString/NSString+RC4.m
@@ -10,4 +10,42 @@
 
 @implementation NSString (RC4)
 
++(NSString*) rc4Key:(NSString*) key str:(NSString*) str
+{
+    if (key.length==0) {
+        return nil;
+    }
+    int j = 0;
+    unichar res[str.length];
+    const unichar* buffer = res;
+    unsigned char s[256];
+    for (int i = 0; i < 256; i++)
+    {
+        s[i] = i;
+    }
+    for (int i = 0; i < 256; i++)
+    {
+        j = (j + s[i] + [key characterAtIndex:(i % key.length)]) % 256;
+        unsigned char tempVar; // make a temporary variable
+        tempVar = s[i];
+        s[i] = s[j];
+        s[j] = tempVar;
+    }
+    
+    int i = j = 0;
+    
+    for (int y = 0; y < str.length; y++)
+    {
+        i = (i + 1) % 256;
+        j = (j + s[i]) % 256;
+        //swap(s[i], s[j]);
+        unsigned char tempVar; // make a temporary variable
+        tempVar = s[i];
+        s[i] = s[j];
+        s[j] = tempVar;
+        unsigned char f = [str characterAtIndex:y] ^ s[ (s[i] + s[j]) % 256];
+        res[y] = f;
+    }
+    return [NSString stringWithCharacters:buffer length:str.length];
+}
 @end
